606981a0f6859d2b9aa83938a7cc2d79fa3d8633
Danny Vernovsky <dvernovsky@interwise.com>
2018-06-07 19:02:45 +0300

    ✅ added tests for user components

--- src/app/pages/user/user.component.spec.ts
+++ src/app/pages/user/user.component.spec.ts
@@ -1,21 +1,57 @@
-import { UserComponent } from './user.component';
+jest.mock('./services/user.service');
+
 import { TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { MatHeaderRowDef, MatDialogModule, MatRowDef} from '@angular/material';
-import {UserService} from './services/user.service';
-import {HttpClientModule} from '@angular/common/http';
-import {Shallow} from 'shallow-render';
-import {SidebarComponent} from '../../components/sidebar/sidebar.component';
-import {ComponentsModule} from '../../components/components.module';
-import {AppModule} from '../../app.module';
+import { MatHeaderRowDef, MatRowDef, MatHeaderRow, MatDialog } from '@angular/material';
+import { UserComponent } from './user.component';
+import { UserService } from './services/user.service';
+
 describe('dashboard component', () => {
-  let shallow: Shallow<UserComponent>;
   beforeEach(async () => {
-    shallow = new Shallow(UserComponent, AppModule);
+
+    class UserDialogMock {
+      open = jest.fn().mockImplementation(() => {
+        return {
+          afterClosed: () => {
+            return {
+              subscribe: jest.fn(),
+            };
+          },
+        };
+      });
+    }
+
+    TestBed.configureTestingModule({
+      imports: [
+
+      ],
+      declarations: [
+        UserComponent,
+        MatHeaderRow,
+        MatRowDef,
+        MatHeaderRowDef,
+      ],
+      providers: [
+        UserService,
+        { provide: MatDialog, useClass: UserDialogMock },
+
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
+    });
+
+    // shallow = new Shallow(UserComponent, AppModule);
   });
 
   it('should render component as described in snapshot', () => {
-    const userComponent = shallow.render('<app-user></app-user>');
-    expect(userComponent).toMatchSnapshot();
+    const fixture = TestBed.createComponent(UserComponent);
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should open dialog when calling addNewUser function', () => {
+    const fixture = TestBed.createComponent(UserComponent);
+    fixture.componentInstance.addNewUser();
+
+    const userDialogMock = TestBed.get(MatDialog);
+    expect(userDialogMock.open).toHaveBeenCalled();
   });
 });
