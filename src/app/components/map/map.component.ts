2007d71bf758e7bdda6f128d06f6b32fe54807bb
Naffar, Nabil (nn605g) <nn605g@intl.att.com>
2018-06-13 16:54:08 +0300

    [school-map] -displaying floor numbers

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { LatLng, latLng, polyline } from 'leaflet';
+import { LatLng, latLng, polyline, Polyline } from 'leaflet';
 import { DEFAULT_MAP_OPTIONS } from './map.config';
 import { PATHS } from './paths.mock';
+import { LineString, MultiLineString } from 'geojson';
 @Component({
     selector: 'app-map',
     templateUrl: './map.component.html',
@@ -11,19 +12,40 @@ export class MapComponent implements OnInit {
     options = DEFAULT_MAP_OPTIONS;
     center: LatLng | undefined;
     currentFloor = 1;
-    layers = PATHS.edges
-        .filter((edge) => this.isEdgeInFloor(PATHS, edge, this.currentFloor))
-        .map((edge) => polyline([
-                latLng(PATHS.nodes[edge.begin].latitude, PATHS.nodes[edge.begin].longitude),
-                latLng(PATHS.nodes[edge.end].latitude, PATHS.nodes[edge.end].longitude),
-            ]));
+    layers: Polyline<LineString | MultiLineString, any>[];
+    availableFloors: number[];
 
     ngOnInit() {
+        this.availableFloors = this.getAvailableFloors();
+        this.calcLayers();
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition((position) => {
                 this.center = latLng(position.coords.latitude, position.coords.longitude);
             });
         }
+    }
+
+    getAvailableFloors() {
+        return PATHS.nodes.reduce((floors, node) => {
+            if (floors.indexOf(node.floor) === -1) {
+                floors.push(node.floor);
+            }
+            return floors;
+        }, [] as number[]).sort();
+    }
+
+    setFloor(floorNum: number) {
+        this.currentFloor = floorNum;
+        this.calcLayers();
+    }
+
+    calcLayers() {
+        this.layers = PATHS.edges
+            .filter((edge) => this.isEdgeInFloor(PATHS, edge, this.currentFloor))
+            .map((edge) => polyline([
+                latLng(PATHS.nodes[edge.begin].latitude, PATHS.nodes[edge.begin].longitude),
+                latLng(PATHS.nodes[edge.end].latitude, PATHS.nodes[edge.end].longitude),
+            ]));
     }
 
     private isEdgeInFloor(paths: typeof PATHS, edge: { begin: number; end: number; }, floor: number): boolean {
