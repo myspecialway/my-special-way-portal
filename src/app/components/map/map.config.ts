6e613777eb01295d907dc9c8232b65b06bfddc1b
Naffar, Nabil (nn605g) <nn605g@intl.att.com>
2018-06-18 18:01:32 +0300

    [POIs] - added waypoints data

--- src/app/components/map/map.config.ts
+++ src/app/components/map/map.config.ts
@@ -30,3 +30,26 @@ export const DEFAULT_MAP_OPTIONS = {
     center: latLng(DEFAULT_POSITION.latitude, DEFAULT_POSITION.longtitude),
 };
 
+export const WAYPOINTS = [
+    {
+        name: 'Room1',
+        disabled: false,
+        position: {latitude: 31.98637522, longtitude: 34.9106703, floor: 2}
+    },
+    {
+        name: 'Room2',
+        disabled: false,
+        position: {latitude: 31.98633199, longtitude: 34.91070315, floor: 2}
+    },
+    {
+        name: 'Room3',
+        disabled: false,
+        position: {latitude: 31.9863081, longtitude: 34.91068572, floor: 3}
+    },
+    {
+        name: 'Room4',
+        disabled: false,
+        position: {latitude: 31.98637522, longtitude: 34.9106703, floor: 3}
+    },
+];
+
