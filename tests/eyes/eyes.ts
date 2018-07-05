bae60df71489b7119cf5588a2df16ef6bc3fe02e
David Khanin <david.khanin@gmail.com>
2018-07-05 11:22:23 +0300

    Adding support for visual Web page tests via Applitools (#71)
    Adding support for visual Web page tests via Applitools
    Currently ignoring tables for the tests not to be flaky
    later need to learn to do it properly.

--- tests/eyes/eyes.ts
+++ tests/eyes/eyes.ts
@@ -1,16 +1,26 @@
-import { Eyes, ConsoleLogHandler } from 'eyes.images';
+import { Eyes } from 'eyes.images'; // ConsoleLogHandler
+import * as readFilePromise from 'fs-readfile-promise';
 
-export class NavbarComponent  {
-    // Initialize the eyes SDK and set your private API key.
+export class EyesDriver  {
+
     eyes = new Eyes();
     constructor() {
-        this.eyes.setApiKey('O4GjwYs66106Ycj0xINUrtv3bwmtcoOP97bprJVCL04XFo110');
-        this.eyes.setLogHandler(new ConsoleLogHandler(true));
-        this.eyes.setOs('Windows 7');
-    }
+            this.eyes.setApiKey('4ZzDF102xhdjJa3012AJPueEC4BR0I7kcoCqVGbKjFXCk110');
+           //  this.eyes.setLogHandler(new ConsoleLogHandler(true));
+            this.eyes.setOs('Windows 7');
+        }
 
-    checkImage(img) {
-            return this.eyes.checkImage(img, 'Contact-us page');
+    async look(t, page: string) {
+        if (process.env.CI) {
+        await t.takeScreenshot(page.replace(/ /g, ''));
+        const image = await readFilePromise(__dirname + '/screenshots/' + page.replace(/ /g, '') + '.png');
+        await this.eyes.checkImage(image, page);
+        }
     }
-
+    async openEyes(str) {
+        await this.eyes.open('Image test', str, {width: 1000, height: 800});
+    }
+    async closeEyes() {
+        await this.eyes.close();
+    }
 }
