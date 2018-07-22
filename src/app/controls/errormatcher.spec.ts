b2bb8392d92ddb3bcb5b0f3c09d04fc0a02a111b
dkhnn <david.khanin@gmail.com>
2018-07-22 20:19:33 +0300

    cleaner UT

--- src/app/controls/errormatcher.spec.ts
+++ src/app/controls/errormatcher.spec.ts
@@ -4,44 +4,29 @@ import { FormControl, Validators } from '@angular/forms';
 describe('error matcher', () => {
 
   beforeEach(() => {
+    const matcher = new MswErrorStateMatcher();
 
   });
 
   it('should return false of formcontrol has error but is not dirty, touched or submitted', async () => {
-    // given
     const formControl = new FormControl('', [Validators.required]);
-    const matcher = new MswErrorStateMatcher();
-    // when
-
-    expect(matcher.isErrorState(formControl, null)).toBe(false);
+    expect(this.matcher.isErrorState(formControl, null)).toBe(false);
   });
 
   it('should return true if formcontrol has error and is dirty', async () => {
-    // given
     const formControl = new FormControl('', [Validators.required]);
     formControl.markAsDirty();
-    const matcher = new MswErrorStateMatcher();
-    // when
-
-    expect(matcher.isErrorState(formControl, null)).toBe(true);
+    expect(this.matcher.isErrorState(formControl, null)).toBe(true);
   });
   it('should return true if formcontrol has error and is dirty', async () => {
-    // given
     const formControl = new FormControl('', [Validators.required]);
     formControl.markAsTouched();
-    const matcher = new MswErrorStateMatcher();
-    // when
-
-    expect(matcher.isErrorState(formControl, null)).toBe(true);
+    expect(this.matcher.isErrorState(formControl, null)).toBe(true);
   });
   it('should return false if formcontrol has no error and is tauched', async () => {
-    // given
     const formControl = new FormControl('asd', [Validators.required]);
     formControl.markAsTouched();
-    const matcher = new MswErrorStateMatcher();
-    // when
-
-    expect(matcher.isErrorState(formControl, null)).toBe(false);
+    expect(this.matcher.isErrorState(formControl, null)).toBe(false);
   });
 
 });
