5804d66f84c17d5289fab090018c1a47b6eb6b93
Danny Vernovsky <dvernovsky@interwise.com>
2018-07-31 17:14:01 +0300

    💚 added exception handling on reading screenshot

--- tests/eyes/eyes.ts
+++ tests/eyes/eyes.ts
@@ -1,26 +1,30 @@
 import { Eyes } from 'eyes.images'; // ConsoleLogHandler
 import * as readFilePromise from 'fs-readfile-promise';
 
-export class EyesDriver  {
+export class EyesDriver {
 
-    eyes = new Eyes();
-    constructor() {
-            this.eyes.setApiKey('4ZzDF102xhdjJa3012AJPueEC4BR0I7kcoCqVGbKjFXCk110');
-           //  this.eyes.setLogHandler(new ConsoleLogHandler(true));
-            this.eyes.setOs('Windows 7');
-        }
+  eyes = new Eyes();
+  constructor() {
+    this.eyes.setApiKey('4ZzDF102xhdjJa3012AJPueEC4BR0I7kcoCqVGbKjFXCk110');
+    //  this.eyes.setLogHandler(new ConsoleLogHandler(true));
+    this.eyes.setOs('Windows 7');
+  }
 
-    async look(t, page: string) {
-        if (process.env.CI) {
-        await t.takeScreenshot(page.replace(/ /g, ''));
+  async look(t, page: string) {
+    if (process.env.CI) {
+      await t.takeScreenshot(page.replace(/ /g, ''));
+      try {
         const image = await readFilePromise(__dirname + '/screenshots/' + page.replace(/ /g, '') + '.png');
         await this.eyes.checkImage(image, page);
-        }
+      } catch (error) {
+        console.error(`e2etests:: eyes:: error reading screenshot ${error}`);
+      }
     }
-    async openEyes(str) {
-        await this.eyes.open('Image test', str, {width: 1000, height: 800});
-    }
-    async closeEyes() {
-        await this.eyes.close();
-    }
+  }
+  async openEyes(str) {
+    await this.eyes.open('Image test', str, { width: 1000, height: 800 });
+  }
+  async closeEyes() {
+    await this.eyes.close();
+  }
 }
