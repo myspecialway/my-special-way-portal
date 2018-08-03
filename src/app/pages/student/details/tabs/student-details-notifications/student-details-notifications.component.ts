ebab81f678c44eb57a5fefc64ed63442e8eb3ad3
Danny Vernovsky <dvernovsky@interwise.com>
2018-08-03 08:44:14 +0300

    🚨 linter errors

--- src/app/pages/student/details/tabs/student-details-notifications/student-details-notifications.component.ts
+++ src/app/pages/student/details/tabs/student-details-notifications/student-details-notifications.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'apollo-client/util/Observable';
 
 @Component({
   selector: 'app-student-details-notifications',
