4abe4a616dc6365cc5349858730702d4998fd223
as167f <as167f@intl.att.com>
2018-08-20 08:55:26 +0300

    [1234] add user role to navigation menu and filter it by current login user type

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,13 +3,14 @@ import { RouteInfo } from './models/route-info.model';
 import { Apollo } from 'apollo-angular';
 import { GET_USER_PROFILE } from '../../apollo/state/queries/get-user-profile.query';
 import { UserProfileStateModel } from '../../apollo/state/resolvers/state.resolver';
+import {UserRole, UserType} from '../../models/user.model';
 
 export const ROUTES: RouteInfo[] = [
-  { path: 'student', title: 'ניהול תלמידים', class: 'nb-student' },
-  { path: 'class', title: 'ניהול כיתות', class: 'nb-class' },
-  { path: 'lesson', title: 'ניהול שיעורים', class: 'nb-lesson' },
-  { path: 'map', title: 'ניהול מפה', class: 'nb-map' },
-  { path: 'user', title: 'ניהול משתמשים', class: 'nb-user' },
+  { path: 'student', title: 'ניהול תלמידים', class: 'nb-student', roles: [UserType.PRINCIPLE, UserType.TEACHER] },
+  { path: 'class', title: 'ניהול כיתות', class: 'nb-class', roles: [UserType.PRINCIPLE, UserType.TEACHER]},
+  { path: 'lesson', title: 'ניהול שיעורים', class: 'nb-lesson', roles: [UserType.PRINCIPLE, UserType.TEACHER] },
+  { path: 'map', title: 'ניהול מפה', class: 'nb-map' , roles: [UserType.PRINCIPLE]},
+  { path: 'user', title: 'ניהול משתמשים', class: 'nb-user' , roles: [UserType.PRINCIPLE]},
 
 ];
 @Component({
@@ -32,9 +33,10 @@ export class NavbarComponent implements OnInit {
       query: GET_USER_PROFILE,
     }).valueChanges.subscribe((userProf) => {
       this.currentUser = userProf.data.userProfile.username;
+      const currentType = userProf.data.userProfile.role;
+      this.menuItems = ROUTES.filter((menuItem) => menuItem.roles.includes(UserType[currentType]));
+      this.selectedMenuItem = (this.menuItems.length > 0) ? this.menuItems[0].path : ' ';
     });
-    this.menuItems = ROUTES.filter((menuItem) => menuItem); // TODO: filter by role
-    this.selectedMenuItem = 'student';
   }
 
   selectMenuItem(menuItem) {
