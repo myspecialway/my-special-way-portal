de797e3d8a86b3f6da4bd8ef4daf303b07a6eb81
cz324b (cz324b) <cz324b@intl.att.com>
2018-09-02 13:51:22 +0300

    [191] Remove pagination from class table Reviewer: NA

--- src/app/pages/class/class.component.ts
+++ src/app/pages/class/class.component.ts
@@ -4,13 +4,7 @@ import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
-import { merge } from 'rxjs/observable/merge';
-import { of as observableOf } from 'rxjs/observable/of';
-import { catchError } from 'rxjs/operators/catchError';
-import { map } from 'rxjs/operators/map';
-import { startWith } from 'rxjs/operators/startWith';
-import { switchMap } from 'rxjs/operators/switchMap';
-import { MatTableDataSource, MatPaginator, MatSort, MatDialog } from '@angular/material';
+import { MatTableDataSource, MatSort, MatDialog } from '@angular/material';
 import { ClassService } from './services/class.graphql.service';
 import { AddClassDialogComponent } from './dialogs/add/add-class.dialog';
 import { Class } from '../../models/class.model';
@@ -23,13 +17,12 @@ import { UpdateClassDialogComponent } from './dialogs/update/update-class.dialog
   templateUrl: './class.component.html',
   styleUrls: ['./class.component.scss'],
 })
-export class ClassComponent implements OnInit, AfterViewInit {
+export class ClassComponent implements OnInit {
 
   displayedColumns = ['classname', 'level', 'editDetails', 'deleteClass'];
   dataSource = new MatTableDataSource<Class>();
   resultsLength = 0;
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild('table') table: ElementRef;
 
@@ -38,33 +31,21 @@ export class ClassComponent implements OnInit, AfterViewInit {
     public dialog: MatDialog,
   ) { }
 
-  ngOnInit(): void {
-    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
-    merge(this.sort.sortChange, this.paginator.page)
-      .pipe(
-        startWith({}),
-        switchMap(() => {
-            return this.classService.getAllClasses();
-        }),
-        map((data: any) => {
-          return data;
-        }),
-        catchError((err: TypeError) => {
-          console.warn('class.component::ngInInit:: empty stream recieved');
-          return observableOf([]);
-        }),
-    ).subscribe((data) => {
-      if (data) {
-        this.dataSource.data = [...data];
-      }
-    });
-
-  }
-
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
+  async ngOnInit() {
+    await this.populateDatasource();
     this.dataSource.sort = this.sort;
   }
+
+  private async populateDatasource() {
+    try {
+      this.dataSource.data = await this.classService.getAllClasses();
+    } catch (error) {
+      // TODO: implement error handling on UI
+      console.error('Error handling not implemented');
+      throw error;
+    }
+  }
+
   applyFilter(filterValue: string) {
     filterValue = filterValue.trim(); // Remove whitespace
     filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
@@ -83,11 +64,11 @@ export class ClassComponent implements OnInit, AfterViewInit {
         this.classService.create(newClass)
           .then(() => {
             this.dataSource.data.push(newClass);
-            this.dataSource.paginator = this.paginator;
           });
       }
     });
   }
+
   deleteClass(_id: string, name: string, level: string) {
     const dialogRef = this.dialog.open(DeleteClassDialogComponent, {
       data: {_id, name, level},
@@ -99,11 +80,11 @@ export class ClassComponent implements OnInit, AfterViewInit {
           .then(() => {
             const index = _.findIndex(this.dataSource.data, (user) =>  user._id === _id);
             this.dataSource.data.splice(index, 1);
-            this.dataSource.paginator = this.paginator;
           });
       }
     });
   }
+
   editClass(_id: string, name: string, level: string) {
     const dialogRef = this.dialog.open(UpdateClassDialogComponent, {
       data: {_id, name, level},
@@ -120,11 +101,11 @@ export class ClassComponent implements OnInit, AfterViewInit {
           .then((data) => {
             const index = _.findIndex(this.dataSource.data, (user) => user._id === _id);
             this.dataSource.data[index] = _.assign({}, relevantClass, result);
-            this.dataSource.paginator = this.paginator;
           });
       }
     });
   }
+
   _createNewClass(classData: any): Class {
     const _class: Class = new Class();
     _class.name = classData.name;
