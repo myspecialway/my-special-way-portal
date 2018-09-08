15aa8e04ddd85b5e68ea1ecf44f78306ad1e79b7
Leiser, Eran (el091q) <el091q@intl.att.com>
2018-09-08 08:09:15 +0300

    FIx tslint issues

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClassDetailsViewComponent } from './class-details/class-details.view/class-details.view.component';
 import { ClassDetailsContainerComponent } from './class-details/class-details.container/class-details.container.component';
-import { LessonComponent } from './lesson/lesson.component';
 import { ComponentsModule } from '../components/components.module';
 import { RouterModule } from '@angular/router';
 import { MatInputModule, MatSelectModule, MatTableModule, MatTabsModule, MatButtonToggleModule } from '@angular/material';
