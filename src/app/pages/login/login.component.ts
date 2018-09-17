4588a5ac81621b62bc7fe0019a588b346f0f972a
Barzilay, Oshri (ob143h) <ob143h@intl.att.com>
2018-09-17 14:57:31 +0300

    [ Login page ] Credentials  now changed to required

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthenticationService } from '../../services/authentication/authentication.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
   model: any = {};
@@ -13,12 +14,19 @@ export class LoginComponent implements OnInit {
   returnUrl: string;
   rememberMe: boolean;
   loginFailed = false;
+  options: FormGroup;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private authenticationService: AuthenticationService,
-  ) { }
+    private fb: FormBuilder,
+  ) {
+    this.options = fb.group({
+      hideRequired: true,
+      floatLabel: 'auto',
+    });
+  }
 
   ngOnInit(): void {
     this.authenticationService.logout();
