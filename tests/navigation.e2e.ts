3292631dfcc204e7255fffba52444286829dc8e3
Itzik Liderman (il883e) <il883e@intl.att.com>
2018-09-25 17:04:08 +0300

    restricting pages to roles.

--- tests/navigation.e2e.ts
+++ tests/navigation.e2e.ts
@@ -56,7 +56,7 @@ test('teacher navigates to students (default) screen after login', async (t) => 
   await t.expect(studentPage._id().exists).ok();
 });
 
-test('teacher can navigate to authorized pages from navbar', async (t) => {
+test('teacher can navigate to authorized pages only, from navbar', async (t) => {
   await loginPage.loginAsTeacher();
   // allowed
   await navbar.navigateToStudentsPage();
@@ -70,15 +70,12 @@ test('teacher can navigate to authorized pages from navbar', async (t) => {
   await t.expect(navbar.menuDropDownClasses().exists).notOk();
 });
 
-test('teacher can navigate to authorized pages only, by url', async (t) => {
+test('teacher can navigate to authorized pages only, by url (deep linking)', async (t) => {
   await loginPage.loginAsTeacher();
   // allowed
   await t
     .navigateTo(StudentPage.url)
     .expect(studentPage._id().exists)
-    .ok()
-    .navigateTo(ClassesPage.url)
-    .expect(classesPage._id().exists)
     .ok()
     //navigate to a specific teacher's class
     .navigateTo(`${ClassesPage.url}/5ba10adc9c9b4e6c731d62b7`)
@@ -86,10 +83,28 @@ test('teacher can navigate to authorized pages only, by url', async (t) => {
     .ok()
 
     // not allowed
+    .navigateTo(ClassesPage.url)
+    .expect(classesPage._id().exists)
+    .notOk()
+    // we should be thrown to login page for not allowed pages.
+    // but the url includes 'return url' which won't let the teacher login.
+    // so we need to navigate to login page again.
+    .navigateTo(loginPage.url);
+  await loginPage.loginAsTeacher();
+  await waitForDefaultPage();
+  await t
     .navigateTo(LessonsPage.url)
     .expect(lessonsPage._id().exists)
     .notOk()
+    .navigateTo(loginPage.url);
+  await loginPage.loginAsTeacher();
+  await waitForDefaultPage();
+  await t
     .navigateTo(UsersPage.url)
     .expect(usersPage._id().exists)
     .notOk();
 });
+
+const waitForDefaultPage = async () => {
+  await studentPage._id().exists;
+};
