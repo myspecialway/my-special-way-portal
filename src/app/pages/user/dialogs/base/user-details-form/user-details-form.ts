5fcb62cdd8996c1b86c1d48711b7ce972cfac63f
Barzilay, Oshri (ob143h) <ob143h@intl.att.com>
2018-09-26 15:48:10 +0300

    minor refact to user role and class : add UserTypeKey type instead of plain string and starting to support class selection from user's DB record

--- src/app/pages/user/dialogs/base/user-details-form/user-details-form.ts
+++ src/app/pages/user/dialogs/base/user-details-form/user-details-form.ts
@@ -5,6 +5,8 @@ import { UserService } from '../../../services/user.service';
 import { ClassService } from '../../../../class/services/class.graphql.service';
 import { Class } from '../../../../../models/class.model';
 
+export type UserTypeKey = keyof typeof UserType;
+
 @Component({
   selector: 'app-user-details-form',
   templateUrl: './user-details-form.html',
@@ -15,11 +17,11 @@ export class UserDetailsFormComponent implements OnInit, OnDestroy {
   roles: string[];
   userRoleEnum = UserType;
   classes: Class[];
-  currentRole: string;
+  currentRole: UserTypeKey;
   formControl = new FormControl('', [Validators.required]);
   EmailFormControl = new FormControl('', [Validators.required, Validators.email]);
   selectUserType = new FormControl(null, Validators.required);
-  selectGrade = new FormControl({ disabled: this.currentRole !== 'TEACHER' }, [Validators.required]);
+  selectGrade = new FormControl({ disabled: this.getClassDisabled() }, Validators.required);
   userNameFormControl = new FormControl('', [
     Validators.required,
     Validators.minLength(5),
@@ -28,8 +30,12 @@ export class UserDetailsFormComponent implements OnInit, OnDestroy {
 
   @Input()
   options = { data: {}, submitButtonLabel: 'הוסף' };
-  @Input()
   data: User;
+  @Input('data')
+  set _data(value: User) {
+    this.data = value;
+    this.currentRole = (value.role as any) as UserTypeKey;
+  }
 
   @Output()
   formSubmit = new EventEmitter<User>();
@@ -52,12 +58,16 @@ export class UserDetailsFormComponent implements OnInit, OnDestroy {
     });
   }
 
-  onUserTypeChange(event): void {
-    this.currentRole = event;
-    if (event === 'MANAGER') {
+  onUserTypeChange(userType: UserTypeKey): void {
+    this.currentRole = userType;
+    if (this.userRoleEnum[userType] === this.userRoleEnum.PRINCIPLE && this.data.class) {
       this.data.class = undefined;
     }
   }
+  getClassDisabled() {
+    return this.userRoleEnum[this.currentRole] !== this.userRoleEnum.TEACHER;
+  }
+
   async getClasses() {
     this.classes = await this.classService.getAllClasses();
   }
@@ -68,6 +78,10 @@ export class UserDetailsFormComponent implements OnInit, OnDestroy {
 
   submitForm() {
     this.formSubmit.emit(this.form.value);
+  }
+
+  compareClass(c1: Class, c2: Class): boolean {
+    return c1 && c2 ? c1._id === c2._id : c1 === c2;
   }
 
   ngOnDestroy() {}
