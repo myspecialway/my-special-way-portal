7dd177f6a7e578d6fffaa83b224b5cc9cada418e
Rechtman, Maayan(mr372e) <mr372e@intl.att.com>
2018-09-26 10:45:44 +0300

    #235 - Added first e2e test - popup should be showen when clicking on a cell

--- src/app/pages/student/details/student-details.component.ts
+++ src/app/pages/student/details/student-details.component.ts
@@ -13,9 +13,9 @@ export class StudentDetailsComponent implements OnInit {
 
   constructor(private route: ActivatedRoute) {
     this.links = [
-      { label: 'פרטים אישיים', path: './personalInfo' , dataTestId: 'personal-info-tab'},
-      { label: 'מערכת שעות', path: './hours', dataTestId: 'schedule-tab'},
-      { label: 'תזכורות', path: './notifications', dataTestId: 'reminders-tab'},
+      { label: 'פרטים אישיים', path: './personalInfo', dataTestId: 'personal-info-tab' },
+      { label: 'מערכת שעות', path: './hours', dataTestId: 'schedule-tab' },
+      { label: 'תזכורות', path: './notifications', dataTestId: 'reminders-tab' },
     ];
     this.activeLink = this.links[0].label;
   }
--- tests/student.schedule.e2e.ts
+++ tests/student.schedule.e2e.ts
@@ -1,14 +1,15 @@
 import LoginPage from './pageobjects/login.po';
 import NavbarPage from './pageobjects/navbar.po';
-import {testEnvironment} from './config/config';
+import { testEnvironment } from './config/config';
 import StudentPage from './pageobjects/student.po';
 
 const loginPage = new LoginPage();
 const navbar = new NavbarPage();
 const studentPage = new StudentPage();
 
-fixture(`Student tests`).page(testEnvironment.feUrl)
-  .beforeEach( async (t) => {
+fixture(`Student tests`)
+  .page(testEnvironment.feUrl)
+  .beforeEach(async (t) => {
     await t
       .typeText(loginPage.useranmeField, 'principle')
       .typeText(loginPage.passwordField, 'Aa123456')
@@ -17,32 +18,30 @@ fixture(`Student tests`).page(testEnvironment.feUrl)
       .click(navbar.menuDropDownStudents);
     //If the user still exists - delete it.
     if (await studentPage.scheduleTestUserNameCell.exists) {
-      await t
-        .click(studentPage.scheduleTestUserDeleteButton)
-        .click(studentPage.confirmDeleteButton);
+      await t.click(studentPage.scheduleTestUserDeleteButton).click(studentPage.confirmDeleteButton);
     }
     //If the user still exists - fail the test
     await t.expect(studentPage.scheduleTestUserNameCell.exists).notOk();
     //Create a new scheduleTestUser
     await t.click(studentPage.newStudentButton);
     await studentPage.firstName();
+    await t.typeText(studentPage.username, 'scheduleTestUser');
+    await t.typeText(studentPage.password, 'scheduleTestUser');
+    await t.typeText(studentPage.firstName, 'scheduleTestUser');
+    await t.typeText(studentPage.lastName, 'scheduleTestUser');
+    await t.click(studentPage.classId);
+    await t.click(studentPage.classIdFirstChoice);
+    await t.click(studentPage.saveButton);
+
+    //user should now exist
+    await t.expect(studentPage.scheduleTestUserNameCell.exists).ok();
+
+    //Navigate top the schedule screen
+    await t.click(studentPage.scheduleTestUserNameCell);
+    await t.click(studentPage.scheduleTab);
   });
 
-test('schedule for student is displayed correctly', async (t) => {
-
-  await t.click(studentPage.firstName)
-    .click(studentPage.lastName)
-    .click(studentPage.classId)
-    .click(studentPage.password) // just to close the drop down
-    .click(studentPage.password)
-    .click(studentPage.username)
-    .click(studentPage.password); // just so we leave the previous field.
-  await studentPage.firstNameErr().exists;
-  await studentPage.lastNameErr().exists;
-  await studentPage.usernameErr().exists;
-  await studentPage.classIdErr().exists;
-  await studentPage.passwordErr().exists;
-  await t.typeText(studentPage.username, 'a');
-  await studentPage.usernameFormatErr().exists;
-  await t.expect(studentPage.saveButton.hasAttribute('disabled')).ok();
+test('should open popup on click on cell', async (t) => {
+  await t.click(studentPage.scheduleCell);
+  await t.expect(studentPage.editCellDialogue.exists).ok();
 });
