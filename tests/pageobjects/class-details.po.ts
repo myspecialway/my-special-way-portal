b0ab08a0b04e8337d01f8f60257168ca1cf66592
Itzik Liderman (il883e) <il883e@intl.att.com>
2018-10-04 18:42:46 +0300

    classes page e2e tests.

--- tests/pageobjects/class-details.po.ts
+++ tests/pageobjects/class-details.po.ts
@@ -2,7 +2,17 @@ import { Selector } from 'testcafe';
 
 export default class ClassDetailsPage {
   _id: Selector;
+  classNameInput: Selector;
+  gradeSelect: Selector;
+  gradeSelectOption: Selector;
+  backToClassButton: Selector;
+  classRow: Selector;
   constructor() {
     this._id = Selector('[data-test-id$="class-details-page"]');
+    this.classNameInput = Selector('[data-test-id$="class-name-input"]');
+    this.gradeSelect = Selector('[data-test-id$="grade-select"]');
+    this.gradeSelectOption = Selector('.grade-select-option');
+    this.backToClassButton = Selector('.back');
+    this.classRow = Selector('.mat-row');
   }
 }
--- tests/pageobjects/classes.po.ts
+++ tests/pageobjects/classes.po.ts
@@ -3,7 +3,18 @@ import { Selector } from 'testcafe';
 export default class ClassesPage {
   static url = '/class';
   _id: Selector;
+  classNameCell: Selector;
+  addClassButton: Selector;
+  deleteClassDialogDeleteButton: Selector;
+
   constructor() {
     this._id = Selector('[data-test-id$="classes-page"]');
+    this.classNameCell = Selector('.mat-row a');
+    this.addClassButton = Selector('[data-test-id="add-class-button"]');
+    this.deleteClassDialogDeleteButton = Selector('[data-test-id="delete-button"]');
+  }
+
+  static getDeleteClassButtonSelector(className: string): Selector {
+    return Selector(`[data-test-id=delete-class-button-${className}]`);
   }
 }
