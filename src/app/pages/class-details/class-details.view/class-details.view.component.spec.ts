cd7fc2d91afe27d2696c78930d2a027ddac67a12
Itzik Liderman (il883e) <il883e@intl.att.com>
2018-10-07 23:31:22 +0300

    tslint error fix.

--- src/app/pages/class-details/class-details.view/class-details.view.component.spec.ts
+++ src/app/pages/class-details/class-details.view/class-details.view.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ClassDetailsViewComponent } from './class-details.view.component';
 import { NO_ERRORS_SCHEMA } from '@angular/compiler/src/core';
 import { ScheduleService } from '../../../services/schedule/schedule.service';
