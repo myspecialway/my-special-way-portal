47a88aba86d69d72b2d907d84072cd4adec24e27
rmaayan1 <maayan.rechtman@gmail.com>
2018-10-16 15:17:47 +0300

    Fixed e2e tests

--- tests/classes.e2e.ts
+++ tests/classes.e2e.ts
@@ -27,11 +27,11 @@ test('Principle can view all classes', async () => {
 
 test('Principle can add, edit and delete a class', async () => {
   // If the class exists - delete it.
-  while (await classesPage.classNameCell.withExactText('test').exists) {
+  if (await classesPage.classNameCell.withExactText('test').exists) {
     await t.click(getDeleteClassButtonSelector('test')).click(classesPage.confirmDeleteButton);
   }
 
-  while (await classesPage.classNameCell.withExactText('editedClass').exists) {
+  if (await classesPage.classNameCell.withExactText('editedClass').exists) {
     await t.click(getDeleteClassButtonSelector('editedClass')).click(classesPage.confirmDeleteButton);
   }
 
