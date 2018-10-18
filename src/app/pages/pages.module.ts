ac8d77438fdcde1a44d47c7b6d1512aa52f51f59
Rieur, Dorit (dr9081) <dr9081@intl.att.com>
2018-10-18 15:41:50 +0300

    update from creating branch

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClassDetailsViewComponent } from './class-details/class-details.view/class-details.view.component';
 import { ClassDetailsContainerComponent } from './class-details/class-details.container/class-details.container.component';
@@ -12,16 +12,13 @@ import {
   MatTabsModule,
   MatButtonToggleModule,
   MAT_LABEL_GLOBAL_OPTIONS,
-  MatListModule,
-  MatSlideToggleModule,
+  MatIconModule,
 } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { StudentDetailsComponent } from './student/details/student-details.component';
 import { StudentDetailsPersonalInfoComponent } from './student/details/tabs/student-details-personal-info/student-details-personal-info.component';
 import { StudentDetailsHoursComponent } from './student/details/tabs/student-details-hours/student-details-hours.component';
 import { StudentDetailsNotificationsComponent } from './student/details/tabs/student-details-notifications/student-details-notifications.component';
-import { MapContainerComponent } from './maps/maps.container/map.container.component';
-import { MapViewComponent } from './maps/maps.view/map.view.component';
 
 @NgModule({
   imports: [
@@ -34,8 +31,7 @@ import { MapViewComponent } from './maps/maps.view/map.view.component';
     MatSelectModule,
     MatTableModule,
     MatTabsModule,
-    MatListModule,
-    MatSlideToggleModule,
+    MatIconModule,
   ],
   declarations: [
     StudentDetailsComponent,
@@ -44,9 +40,8 @@ import { MapViewComponent } from './maps/maps.view/map.view.component';
     StudentDetailsNotificationsComponent,
     ClassDetailsViewComponent,
     ClassDetailsContainerComponent,
-    MapContainerComponent,
-    MapViewComponent,
   ],
   providers: [{ provide: MAT_LABEL_GLOBAL_OPTIONS, useValue: { float: 'auto' } }],
+  schemas: [NO_ERRORS_SCHEMA],
 })
 export class PagesModule {}
