c511ae752c5bcb399152f3da25d712ba53a879ad
Rieur, Dorit (dr9081) <dr9081@intl.att.com>
2018-10-21 15:11:42 +0300

    updated files from master

--- src/app/services/location/location.graphql.service.ts
+++ src/app/services/location/location.graphql.service.ts
@@ -1,7 +1,34 @@
 import { Injectable } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
-import { LocationQuery } from '../../models/location.model';
+import { InputLocation, LocationQuery } from '../../models/location.model';
+import * as _ from 'lodash';
+
+const allLocationFields = `
+              _id
+              name
+              disabled
+              location_id
+              position {
+                latitude
+                longitude
+                floor
+              }
+            `;
+
+const updateLocation = gql`
+    mutation updateLocation($id: ID!, $location: InputLocation!) {
+      updateLocation(id: $id, location: $location) {
+        ${allLocationFields}
+      }
+    }
+  `;
+
+const getAllLocationsQuery = gql`{
+          locations {
+           ${allLocationFields}
+          }
+        }`;
 
 @Injectable()
 export class LocationService {
@@ -10,22 +37,32 @@ export class LocationService {
   getLocations() {
     return this.apollo
       .query<LocationQuery>({
-        query: gql`
-          {
-            locations {
-              _id
-              name
-              disabled
-              position {
-                latitude
-                longitude
-                floor
-              }
-            }
-          }
-        `,
+        query: getAllLocationsQuery,
       })
       .toPromise()
       .then((res) => res.data.locations);
+  }
+
+  update(location: InputLocation) {
+    return this.apollo
+      .mutate({
+        mutation: updateLocation,
+        variables: {
+          id: location._id,
+          location: _.omit(location, ['_id']),
+        },
+        refetchQueries: [
+          {
+            query: getAllLocationsQuery,
+          },
+        ],
+        awaitRefetchQueries: true,
+      })
+      .toPromise()
+      .then((res) => {
+        if (res.data) {
+          return res.data.locations;
+        }
+      });
   }
 }
