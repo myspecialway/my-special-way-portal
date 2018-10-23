2659f70a50a237fe6bf5152f06b870c91e9879a1
Bali, Doron (db991h) <db991h@intl.att.com>
2018-10-23 22:24:25 +0300

    basic functionality - click and delete

--- src/app/services/lesson/lesson.graphql.service.ts
+++ src/app/services/lesson/lesson.graphql.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
 import { LessonQuery } from '../../models/lesson.model';
+import { FetchResult } from 'apollo-link';
 
 @Injectable()
 export class LessonService {
@@ -22,5 +23,31 @@ export class LessonService {
       })
       .toPromise()
       .then((res) => res.data.lessons);
+  }
+  public delete(lessonId: string): Promise<FetchResult<Record<string, any>>> {
+    return this.apollo
+      .mutate({
+        mutation: gql`
+      mutation {
+        deleteLesson(
+          id: "${lessonId}"
+        )
+    }`,
+        refetchQueries: [
+          {
+            query: gql`
+              {
+                lessons {
+                  _id
+                  title
+                  icon
+                }
+              }
+            `,
+          },
+        ],
+        awaitRefetchQueries: true,
+      })
+      .toPromise();
   }
 }
