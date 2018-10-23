3371216017d04cdd8e7cc053451e52b660c64d70
Bali, Doron (db991h) <db991h@intl.att.com>
2018-10-23 22:24:25 +0300

    basic functionality - click and delete

--- src/app/pages/lesson/lesson.component.ts
+++ src/app/pages/lesson/lesson.component.ts
@@ -1,14 +1,7 @@
-import { MatTableDataSource, MatSort, MatDialog } from '@angular/material';
+import { MatTableDataSource, MatSort } from '@angular/material';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Lesson } from '../../models/lesson.model';
 import { LessonService } from '../../services/lesson/lesson.graphql.service';
-import { SubscriptionCleaner } from '../../decorators/SubscriptionCleaner.decorator';
-import { first } from 'rxjs/operators';
-import { DeleteLessonDialogComponent } from './dialogs/delete/delete-lesson.dialog';
-import { Subscription } from 'rxjs';
-import { ClassService } from '../class/services/class.graphql.service';
-import { Class } from '../../models/class.model';
-import { CantDeleteLessonDialogComponent } from './dialogs/cant-delete/cant-delete-lesson.dialog';
 
 @Component({
   selector: 'app-lesson',
@@ -22,56 +15,16 @@ export class LessonComponent implements OnInit {
   @ViewChild(MatSort)
   sort: MatSort;
 
-  @SubscriptionCleaner()
-  subCollector: Subscription;
-  constructor(private lessonService: LessonService, private classService: ClassService, public dialog: MatDialog) {}
+  constructor(private lessonService: LessonService) {}
+
   async ngOnInit() {
-    try {
-      this.subCollector.add(
-        this.lessonService.getAllLessons().subscribe((lessons) => {
-          this.dataSource.data = [...lessons];
-        }),
-      );
-    } catch (error) {
-      // TODO: implement error handling on UI
-      console.error('Error handling not implemented');
-      throw error;
-    }
+    const lessons = await this.lessonService.getLessons();
+    this.dataSource.data = [...lessons];
   }
 
   addNewLesson() {}
 
-  public async deleteLesson(_id: string, title: string) {
-    const dialogRef = this.dialog.open(DeleteLessonDialogComponent, {
-      data: { _id, title },
-    });
-    this.subCollector.add(
-      dialogRef
-        .afterClosed()
-        .pipe(first())
-        .subscribe(async (result) => {
-          if (result === true) {
-            const getAllClassesSub = this.classService
-              .getAllClasses()
-              .map((classes: Class[]) => this.filterClassesWithLesson(classes, title))
-              .subscribe((classesWithLessonTitle) => {
-                if (classesWithLessonTitle.length === 0) {
-                  this.lessonService.delete(_id);
-                } else {
-                  this.dialog.open(CantDeleteLessonDialogComponent, {
-                    data: { title, className: classesWithLessonTitle[0].name },
-                  });
-                }
-              });
-            this.subCollector.add(getAllClassesSub);
-          }
-        }),
-    );
-  }
-  private filterClassesWithLesson(classes: Class[], lessonTitle: string): Class[] {
-    return classes.filter(
-      (cls: Class) =>
-        cls.schedule.filter((schedule) => schedule.lesson && schedule.lesson.title === lessonTitle).length > 0,
-    );
+  deleteLesson(_id: string) {
+    this.lessonService.delete(_id);
   }
 }
