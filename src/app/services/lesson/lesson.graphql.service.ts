bf90cd70e6fbd3072d409b9a3c2d505ab02f2125
Bali, Doron (db991h) <db991h@intl.att.com>
2018-10-23 22:24:25 +0300

    basic functionality - click and delete

--- src/app/services/lesson/lesson.graphql.service.ts
+++ src/app/services/lesson/lesson.graphql.service.ts
@@ -1,110 +1,49 @@
 import { Injectable } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
-import { LessonQuery, Lesson } from '../../models/lesson.model';
+import { LessonQuery } from '../../models/lesson.model';
 import { FetchResult } from 'apollo-link';
-import { catchError, map } from 'rxjs/operators';
-import { of as observableOf, Observable } from 'rxjs';
-
-const GET_ALL_LESSONS_QUERY = gql`
-  {
-    lessons {
-      _id
-      title
-      icon
-    }
-  }
-`;
-const DELETE_LESSON_QUERY = (lessonId) => gql`
-mutation {
-  deleteLesson(
-    id: "${lessonId}"
-  )
-}`;
-const CREATE_LESSON_QUERY = (title, icon) => gql`
-  mutation {
-    createLesson(lesson: {
-      title: "${title}"
-      icon: "${icon}"
-    }){ _id }
-  }`;
-const UPDATE_LESSON_QUERY = (lessonid, title, icon) => gql`
-mutation {
-  updateLesson(id: "${lessonid}", lesson: {
-    title: "${title}"
-    icon: "${icon}"
-  }){ _id }
-}`;
 
 @Injectable()
 export class LessonService {
-  public create(title: string, icon: string): any {
-    return this.apollo
-      .mutate({
-        mutation: CREATE_LESSON_QUERY(title, icon),
-        refetchQueries: [
-          {
-            query: GET_ALL_LESSONS_QUERY,
-          },
-        ],
-        awaitRefetchQueries: true,
-      })
-      .toPromise()
-      .then((res) => {
-        if (res.data) {
-          return res.data.createClass;
-        }
-      });
-  }
-  public update(lessonId: string, title: string, icon: string): any {
-    return this.apollo
-      .mutate({
-        mutation: UPDATE_LESSON_QUERY(lessonId, title, icon),
-        refetchQueries: [
-          {
-            query: GET_ALL_LESSONS_QUERY,
-          },
-        ],
-        awaitRefetchQueries: true,
-      })
-      .toPromise()
-      .then((res) => {
-        if (res.data) {
-          return res.data.createClass;
-        }
-      });
-  }
   constructor(private apollo: Apollo) {}
-  public getAllLessons(): Observable<Lesson[]> {
-    return this.apollo
-      .watchQuery<{ lessons: Lesson[] }>({
-        query: GET_ALL_LESSONS_QUERY,
-      })
-      .valueChanges.pipe(
-        map((res) => {
-          return res.data.lessons;
-        }),
-        catchError((err: TypeError) => {
-          console.warn('user.component::ngInInit:: empty stream recieved');
-          return observableOf([]);
-        }),
-      );
-  }
+
   getLessons() {
     return this.apollo
       .query<LessonQuery>({
-        query: GET_ALL_LESSONS_QUERY,
+        query: gql`
+          {
+            lessons {
+              _id
+              title
+              icon
+            }
+          }
+        `,
       })
       .toPromise()
       .then((res) => res.data.lessons);
   }
-  public async delete(lessonId: string): Promise<FetchResult<Record<string, any>>> {
+  public delete(lessonId: string): Promise<FetchResult<Record<string, any>>> {
     return this.apollo
       .mutate({
-        mutation: DELETE_LESSON_QUERY(lessonId),
+        mutation: gql`
+      mutation {
+        deleteLesson(
+          id: "${lessonId}"
+        )
+    }`,
         refetchQueries: [
           {
-            query: GET_ALL_LESSONS_QUERY,
+            query: gql`
+              {
+                lessons {
+                  _id
+                  title
+                  icon
+                }
+              }
+            `,
           },
         ],
         awaitRefetchQueries: true,
