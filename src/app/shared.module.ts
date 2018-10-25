8b0b76784b6b0e2b4313a9310c507966564bef29
Roi ben haim (rb136m) <rb136m@att.com>
2018-10-26 00:39:19 +0300

    [Localization] implmented ngx-translate directive & service and started the resource mappings.

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -1,10 +1,11 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { CdkTableModule } from '@angular/cdk/table';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
 import {
   MatButtonModule,
   MatCardModule,
@@ -22,6 +23,7 @@ import {
   MatDividerModule,
   MatSnackBarModule,
 } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
@@ -39,7 +41,6 @@ import { AddStudentDialogComponent } from './pages/student/dialogs/add/add-stude
 import { UserComponent } from './pages/user/user.component';
 import { AddUserDialogComponent } from './pages/user/dialogs/add/add-user.dialog';
 import { UserService } from './pages/user/services/user.service';
-import { BrowserModule } from '@angular/platform-browser';
 import { UserDetailsFormComponent } from './pages/user/dialogs/base/user-details-form/user-details-form';
 import { DeleteUserDialogComponent } from './pages/user/dialogs/delete/delete-user.dialog';
 import { UpdateUserDialogComponent } from './pages/user/dialogs/update/update-user.dialog';
@@ -54,63 +55,44 @@ import { ScheduleService } from './services/schedule/schedule.service';
 import { LocationService } from './services/location/location.graphql.service';
 import { MSWSnackbar } from './services/msw-snackbar/msw-snackbar.service';
 
-import { SharedModule } from './shared.module';
-// AoT requires an exported function for factories
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
-}
-
 @NgModule({
   imports: [
-    SharedModule.forRoot(),
-    BrowserAnimationsModule,
-    BrowserModule,
-    FormsModule,
-    ComponentsModule,
-    RouterModule,
-    AppRoutingModule,
-    CdkTableModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    MatCheckboxModule,
-    MatDividerModule,
-    MSWApolloModule,
-    PagesModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient],
-      },
-    }),
+    // BrowserAnimationsModule,
+    // BrowserModule,
+    // FormsModule,
+
+    // RouterModule,
+    MatButtonModule,
+    MatRippleModule,
+    MatInputModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    // AppRoutingModule,
+    // CdkTableModule,
+    // HttpClientModule,
+    // ReactiveFormsModule,
+    // MatCheckboxModule,
+    // MatDividerModule,
+    // MSWApolloModule,
+    // PagesModule,
+    // TranslateModule.forRoot({
+    //     loader: {
+    //         provide: TranslateLoader,
+    //         useFactory: HttpLoaderFactory,
+    //         deps: [HttpClient],
+    //     },
+    // }),
   ],
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    LoginComponent,
-    ClassComponent,
-    StudentComponent,
-    UserComponent,
-    UserDetailsFormComponent,
-    AddUserDialogComponent,
-    UpdateUserDialogComponent,
-    DeleteUserDialogComponent,
-    AddStudentDialogComponent,
-    UpdateStudentDialogComponent,
-    DeleteStudentDialogComponent,
-    DeleteClassDialogComponent,
-    ScheduleDialogComponent,
-  ],
-  entryComponents: [
-    AddUserDialogComponent,
-    UpdateUserDialogComponent,
-    DeleteUserDialogComponent,
-    AddStudentDialogComponent,
-    UpdateStudentDialogComponent,
-    DeleteStudentDialogComponent,
-    DeleteClassDialogComponent,
-    ScheduleDialogComponent,
-  ],
+  declarations: [],
   providers: [
     AuthGuard,
     AuthenticationService,
@@ -122,6 +104,28 @@ export function HttpLoaderFactory(http: HttpClient) {
     LocationService,
     MSWSnackbar,
   ],
-  bootstrap: [AppComponent],
+  exports: [
+    MatButtonModule,
+    MatRippleModule,
+    MatInputModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatSnackBarModule,
+  ],
 })
-export class AppModule {}
+export class SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: [AuthGuard],
+    };
+  }
+}
