faed41111926da8198c324eff4b102a1b302ebf7
Bali, Doron (db991h) <db991h@intl.att.com>
2018-10-25 09:35:15 +0300

    delete confirmation dialog.

--- src/app/pages/lesson/lesson.component.ts
+++ src/app/pages/lesson/lesson.component.ts
@@ -5,10 +5,6 @@ import { LessonService } from '../../services/lesson/lesson.graphql.service';
 import { SubscriptionCleaner } from '../../decorators/SubscriptionCleaner.decorator';
 import { first } from 'rxjs/operators';
 import { DeleteLessonDialogComponent } from './dialogs/delete/delete-lesson.dialog';
-import { Subscription } from 'rxjs';
-import { ClassService } from '../class/services/class.graphql.service';
-import { Class } from '../../models/class.model';
-import { CantDeleteLessonDialogComponent } from './dialogs/cant-delete/cant-delete-lesson.dialog';
 
 @Component({
   selector: 'app-lesson',
@@ -23,8 +19,9 @@ export class LessonComponent implements OnInit {
   sort: MatSort;
 
   @SubscriptionCleaner()
-  subCollector: Subscription;
-  constructor(private lessonService: LessonService, private classService: ClassService, public dialog: MatDialog) {}
+  subCollector;
+  constructor(private lessonService: LessonService, public dialog: MatDialog) {}
+
   async ngOnInit() {
     try {
       this.subCollector.add(
@@ -41,9 +38,9 @@ export class LessonComponent implements OnInit {
 
   addNewLesson() {}
 
-  public async deleteLesson(_id: string, title: string) {
+  public async deleteLesson(_id: string) {
     const dialogRef = this.dialog.open(DeleteLessonDialogComponent, {
-      data: { _id, title },
+      data: { _id, name, level: null },
     });
     this.subCollector.add(
       dialogRef
@@ -51,27 +48,9 @@ export class LessonComponent implements OnInit {
         .pipe(first())
         .subscribe(async (result) => {
           if (result === true) {
-            const getAllClassesSub = this.classService
-              .getAllClasses()
-              .map((classes: Class[]) => this.filterClassesWithLesson(classes, title))
-              .subscribe((classesWithLessonTitle) => {
-                if (classesWithLessonTitle.length === 0) {
-                  this.lessonService.delete(_id);
-                } else {
-                  this.dialog.open(CantDeleteLessonDialogComponent, {
-                    data: { title, className: classesWithLessonTitle[0].name },
-                  });
-                }
-              });
-            this.subCollector.add(getAllClassesSub);
+            await this.lessonService.delete(_id);
           }
         }),
-    );
-  }
-  private filterClassesWithLesson(classes: Class[], lessonTitle: string): Class[] {
-    return classes.filter(
-      (cls: Class) =>
-        cls.schedule.filter((schedule) => schedule.lesson && schedule.lesson.title === lessonTitle).length > 0,
     );
   }
 }
