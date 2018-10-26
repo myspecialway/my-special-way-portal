903a05a0ea4fac0231e1b4d9d5d690cca227ab26
Leiser, Eran (el091q) <el091q@intl.att.com>
2018-10-26 14:49:19 +0300

    [add scheduled hours] - add hours to each class that is being inserted

--- src/app/pages/student/details/tabs/student-details-hours/student-details-hours.component.ts
+++ src/app/pages/student/details/tabs/student-details-hours/student-details-hours.component.ts
@@ -78,6 +78,7 @@ export class StudentDetailsHoursComponent implements OnInit {
           const onlyCustomizedSlots: TimeSlot[] = this.student.schedule.filter((slot) => slot.customized);
           const newCustomizedSlot: TimeSlot = {
             index: data.index,
+            hours: data.hour,
             lesson: data.lesson,
             location: data.location,
             customized: true,
