11a92674e5e527dcc17d8458375e24b70e131555
oyasovsky <oyasovsky@interwise.com>
2018-11-12 17:37:35 +0200

    cleanup

--- src/app/pages/shared/session-mgmt/session-header/session-header.component.ts
+++ src/app/pages/shared/session-mgmt/session-header/session-header.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-session-header',
