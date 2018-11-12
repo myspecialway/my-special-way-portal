d3bba776e4adaa107b97a5c6fb4db60b62fc18ac
Barzilay, Oshri (ob143h) <ob143h@intl.att.com>
2018-11-12 13:45:47 +0200

    minor type signature added

--- src/app/models/reminder.model.ts
+++ src/app/models/reminder.model.ts
@@ -1,7 +1,5 @@
 import { IReminderTime, IDbReminderTime } from './reminder-time.model';
 
-export type ReminderTypeKey = keyof typeof ReminderType;
-
 export interface IReminder {
   enabled: boolean;
   type: ReminderTypeKey;
@@ -18,8 +16,9 @@ export enum ReminderType {
   MEDICINE = 'תרופה',
   REHAB = 'גמילה',
 }
+export type ReminderTypeKey = keyof typeof ReminderType;
 
-export const getReminderTypeKeys = Object.keys(ReminderType);
+export const getReminderTypeKeys = Object.keys(ReminderType) as ReminderTypeKey[];
 
 export const DEFAULT_REMINDERS: IReminder[] = getReminderTypeKeys.map((key: ReminderTypeKey) => ({
   enabled: false,
