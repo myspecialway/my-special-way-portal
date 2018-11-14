4efa7542b8b6ee989340cb435cf2cdea21668abd
Shitrit, Tal David (ts639c) <ts639c@intl.att.com>
2018-11-14 16:27:27 +0200

    [feat] - same as last commit

--- src/app/pages/student/dialogs/reminders/add/add-student-reminder.dialog.spec.ts
+++ src/app/pages/student/dialogs/reminders/add/add-student-reminder.dialog.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
-import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 import { AddStudentReminderDialogComponent } from './add-student-reminder.dialog';
 import { FormBuilder } from '@angular/forms';
 
