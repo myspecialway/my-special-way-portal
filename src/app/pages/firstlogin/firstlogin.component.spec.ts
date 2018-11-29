165233f53b94e0bb1a507fbf7bd136c493416b60
oyasovsky <oyasovsky@interwise.com>
2018-11-29 08:35:18 +0200

    add some tests

--- src/app/pages/firstlogin/firstlogin.component.spec.ts
+++ src/app/pages/firstlogin/firstlogin.component.spec.ts
@@ -1,42 +1,138 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { FirstloginComponent } from './firstlogin.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { DebugElement } from '@angular/core';
+import 'rxjs/add/observable/of';
+import { classTestData } from '../../../mocks/assets/classes.mock';
 import { UserService } from '../user/services/user.service';
-import { ActivatedRoute } from '@angular/router';
+import { Apollo } from 'apollo-angular';
+import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { NO_ERRORS_SCHEMA } from '@angular/compiler/src/core';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { User, UserType } from '../../models/user.model';
 import { AuthenticationService } from '../../services/authentication/authentication.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { FirstloginComponent } from './firstlogin.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs/observable/of';
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule } from 'apollo-angular';
+import { SessionHeaderComponent } from '../../pages/shared/session-mgmt/session-header/session-header.component';
+import { SessionFooterComponent } from '../../pages/shared/session-mgmt/session-footer/session-footer.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { By } from '@angular/platform-browser';
 
-describe('FirstloginComponent', () => {
+describe('First Login', () => {
   let component: FirstloginComponent;
   let fixture: ComponentFixture<FirstloginComponent>;
-
-  let userServiceMock: Partial<UserService>;
-  let routeMock: Partial<ActivatedRoute>;
-  let authenticationServiceMock: Partial<AuthenticationService>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [FirstloginComponent],
-      providers: [
-        { provide: UserService, useValue: userServiceMock },
-        { provide: ActivatedRoute, useValue: routeMock },
-        { provide: AuthenticationService, useValue: authenticationServiceMock },
-      ],
-      schemas: [NO_ERRORS_SCHEMA],
-    }).compileComponents();
-  }));
+  let authServiceMock: Partial<AuthenticationService>;
 
   beforeEach(() => {
-    userServiceMock = {};
-    routeMock = {};
-    authenticationServiceMock = {};
-
-    fixture = TestBed.createComponent(FirstloginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    authServiceMock = {
+      firstLogin: jest.fn(),
+    };
   });
 
-  xit('should create', () => {
-    expect(component).toBeTruthy();
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule,
+        FormsModule,
+        HttpClientModule,
+        ApolloModule,
+        RouterTestingModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatCheckboxModule,
+      ],
+      declarations: [FirstloginComponent, SessionHeaderComponent, SessionFooterComponent],
+      providers: [FormBuilder, Apollo, UserService, AuthenticationService, HttpClient],
+    }).compileComponents();
+
+    (authServiceMock.firstLogin as jest.Mock).mockImplementationOnce(() => {
+      return of(true);
+    });
+    fixture = TestBed.createComponent(FirstloginComponent);
+    component = fixture.componentInstance;
+  });
+
+  afterEach(() => {
+    if (fixture) fixture.destroy();
+  });
+
+  it('passUpdateFailed to be false', () => {
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(component.passUpdateFailed).toBeFalsy();
+    });
+  });
+
+  it('submitDisabled to be true', () => {
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(component.submitDisabled).toBeTruthy();
+    });
+  });
+
+  it('matchFailed to be false', () => {
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(component.matchFailed).toBeFalsy();
+    });
+  });
+
+  it('hidePassword to be true', () => {
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(component.hidePassword).toBeTruthy();
+    });
+  });
+
+  it('hidePasswordConfirm to be true', () => {
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(component.hidePassword).toBeTruthy();
+    });
+  });
+
+  it('Password match', () => {
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+
+      component.model.password = 'Aa123456';
+      component.model.passwordConfirm = 'Aa123456';
+      expect(component.matchFailed).toBeFalsy();
+    });
+  });
+
+  it('Password mismatch', () => {
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+
+      component.model.password = 'Aa123456';
+      component.model.passwordConfirm = 'Bb123456';
+      expect(component.matchFailed).toBeTruthy();
+    });
+  });
+
+  it('PasswordConfirm is empty', () => {
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+
+      component.model.password = 'Aa123456';
+      component.model.passwordConfirm = '';
+      expect(component.matchFailed).toBeTruthy();
+    });
+  });
+
+  it('Submit button is disabled', () => {
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+
+      component.model.password = 'Bb123456';
+      component.model.passwordConfirm = 'Aa123456';
+      const submitEL: DebugElement = fixture.debugElement.query(By.css('button.btn.btn-main'));
+      expect(submitEL.nativeElement.disabled).toBe(true);
+    });
   });
 });
