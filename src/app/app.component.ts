bd17cbe028b71b134fa5869b141f1c3ed710e8a6
KfirMor <kfir.mor@gmail.com>
2018-12-04 17:38:37 +0200

    UI updates to the Student details page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
 import 'rxjs/add/operator/filter';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+    this.matIconRegistry.addSvgIcon(
+      `male_face`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/icon/male.svg'),
+    );
+    this.matIconRegistry.addSvgIcon(
+      `female_face`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/icon/female.svg'),
+    );
+  }
+}
