1992d1ebd9f4359162b84d15ed91a817a2b4562c
Shitrit, Tal David (ts639c) <ts639c@intl.att.com>
2018-12-18 17:25:53 +0200

    [dev] - deleting comments ............................................................

--- src/app/pages/student/dialogs/reminders/add/add-student-reminder.dialog.spec.ts
+++ src/app/pages/student/dialogs/reminders/add/add-student-reminder.dialog.spec.ts
@@ -18,28 +18,11 @@ describe('AddStudentReminderDialogComponent', () => {
 
   // @ts-ignore
   beforeEach(() => {
-    // let component: AddStudentReminderDialogComponent;
     let fixture: ComponentFixture<AddStudentReminderDialogComponent>;
     fixture = TestBed.createComponent(AddStudentReminderDialogComponent);
-    // component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  // it('should remove an element from the array', () => {
-  //   const array = [1,2,3];
-  //   const index = 1;
-  //   const fixture = TestBed.createComponent(AddStudentReminderDialogComponent);
-  //   fixture.componentInstance.deleteReminder(array, index);
-  //   expect(fixture).toEqual(2);
-  // });
-
   // @ts-ignore
-  it.skip('should close the dialog', () => {
-    // console.log('componentcomponent: ', fixture);
-    // fixture.componentInstance.deleteStudent(123, 'sad', 'asd', 'אשוח', 'MALE');
-    // const DialogMock = TestBed.get(MatDialog);
-    // expect(DialogMock.open).toHaveBeenCalled();
-    // @ts-ignore
-    // expect(thisFixture.componentInstance.close()).toBeDefined();
-  });
+  it.skip('should close the dialog', () => {});
 });
