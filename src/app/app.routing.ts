e6207fc58003d601fc5be9e362d790f0af372e43
Barzilay, Oshri (ob143h) <ob143h@intl.att.com>
2018-12-20 11:13:13 +0200

    tslint fix

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -15,7 +15,6 @@ import { StudentDetailsPersonalInfoComponent } from './pages/student/details/tab
 import { StudentDetailsComponent } from './pages/student/details/student-details.component';
 import { StudentDetailsRemindersComponent } from './pages/student/details/tabs/student-details-reminders/student-details-reminders.component';
 import { UserType } from './models/user.model';
-import { MapContainerComponent } from './pages/maps/maps.container/map.container.component';
 import { FirstloginComponent } from './pages/firstlogin/firstlogin.component';
 import { NonActiveTimeComponent } from './pages/non-active-time/non-active-time.component';
 
