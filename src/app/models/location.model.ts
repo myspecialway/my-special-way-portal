a2bbb554ef3d551be142aea681b4a95fd890e24f
TuliTheProgrammer <oshri.barzilay@gmail.com>
2018-12-23 14:05:11 +0200

    Feature/#367 maps navigation points (#457)
    * [387] adding a new page for the maps blocked sections first part Reviewer: Ivan
    
    * [387] Styling the Maps Control. Tabs, tables, etc. Reviewer: Celine
    
    * [387] Add popup for add block and update block and delete block. Reviewer: NA
    
    * [387] Styling the Maps Modals: Sizes, colors. Reviewer: Celine
    
    * [BugFix] Link to 'http://example.com/' Removed from navbar. Reviewer: Eran
    
    * [387] Typo Fixes, Modal Delet fixes Reviewer: Celine
    
    * [387] Mock Update, Styling Fixes Reviewer: Celine
    
    * initial nav points
    
    * tslint fix
    
    * added demo map miage + right floor list + left nav points list
    
    * remove console logs from reminders
    
    * fix tslint errors
    
    * remove old map  page
    
    * Generelize block delete popup  component
    
    * add delete option + styling map points list
    
    * fxz compilation error
    
    * removing redundant code - following CR  requests
    
    * [387] Integrate the get all blocked sections in maps page Reviewer: NA
    
    * remove redundant subscriptions explicit cleaning
    
    * add floor map delete button
    
    * [387] Integrate the add blocked sections in maps page Reviewer: NA
    
    * [387] Integrate the add blocked sections in maps page Reviewer: NA
    
    * add new map points dialogs
    
    * add "type" property support for map-points + fixing dialogs
    
    * fix last merge (file: app.module.ts)
    
    * tslint fix
    
    * fix merge stage #2
    
    * fix form error
    
    * temp fix for icons
    
    * added locations Mock file
    
    * Update failing Maps Page UnitTests
    
    * tslint fix
    
    * [387] Integrate the update blocked sections in maps page Reviewer: NA
    
    * unit tests fix
    
    * tslint fix
    
    * added new icons
    
    * add onpush for maps components - performance
    
    * hoist Locations value to maps container
    
    * fix points saving to DB
    
    * using mat toggle button for icon selection
    
    * UT fix
    
    * fix map point details edit dialog when saving
    
    * tslint fix
    
    * UT fix
    
    * fix unit tests
    
    * tslint fix
    
    * fix ci error
    
    * tslit fix
    
    * temp build fix - map points dialog

--- src/app/models/location.model.ts
+++ src/app/models/location.model.ts
@@ -1,3 +1,4 @@
+import { IDialogLocation } from './location.model';
 import { Point } from './point.model';
 export interface Location {
   _id: string;
@@ -5,14 +6,39 @@ export interface Location {
   disabled: boolean;
   location_id: string;
   position: Point;
+  type?: string;
+  icon?: string;
 }
 
+export interface IDialogLocation {
+  _id: string;
+  name: string;
+  disabled: boolean;
+  location_id: string;
+  position: Point;
+  type?: string;
+  icon?: string;
+}
+
+export const emptyMapPoint: IDialogLocation = {
+  _id: '',
+  name: '',
+  disabled: false,
+  location_id: '',
+  position: {
+    latitude: 0,
+    longitude: 0,
+    floor: 0,
+  },
+};
 export interface InputLocation {
   _id: string;
   name?: string;
   disabled?: boolean;
   location_id?: string;
   position?: Point;
+  type?: string;
+  icon?: string;
 }
 
 export interface LocationQuery {
---
+++ src/app/pages/maps/maps.container/dialogs/add-update/add-update-block.dialog.ts
@@ -0,0 +1,52 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import BlockedSection from '../../../../../models/blocked-section.model';
+
+interface AddUpdateBlockedSection extends BlockedSection {
+  isNewBlock: boolean;
+}
+
+@Component({
+  selector: 'app-add-update-block.dialog',
+  templateUrl: './add-update-block.dialog.html',
+  styleUrls: ['./add-update-block.dialog.scss'],
+})
+export class AddUpdateBlockDialogComponent implements OnInit {
+  form: FormGroup;
+  formControl = new FormControl('', [Validators.required]);
+  constructor(
+    private formBuilder: FormBuilder,
+    public dialogRef: MatDialogRef<AddUpdateBlockDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: AddUpdateBlockedSection,
+  ) {}
+
+  ngOnInit(): void {
+    if (this.data.isNewBlock) {
+      this.form = this.formBuilder.group({
+        from: '',
+        to: '',
+        reason: '',
+      });
+    } else {
+      this.form = this.formBuilder.group({
+        from: this.data.from,
+        to: this.data.to,
+        reason: this.data.reason,
+        _id: this.data._id,
+      });
+    }
+  }
+
+  getErrorMessage() {
+    return this.formControl.hasError('required') ? 'Required field' : '';
+  }
+
+  close(): void {
+    this.dialogRef.close();
+  }
+
+  confirmAdd(dialogData): void {
+    this.dialogRef.close(dialogData);
+  }
+}
