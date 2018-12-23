d9985919f9db5fbb78439a40561de9ea8fb6ae91
YossiAtt <yg771u@intl.att.com>
2018-12-23 16:05:42 +0200

    [maint] adding e2e test for upload maps

---
+++ tests/maps.e2e.ts
@@ -0,0 +1,52 @@
+import LoginPage from './pageobjects/login.po';
+import { testEnvironment } from './config/config';
+import { t, Selector } from 'testcafe';
+import MapsPage from './pageobjects/maps.po';
+import MapPage from './pageobjects/map-details.po';
+
+const loginPage = new LoginPage();
+const mapsPage = new MapsPage();
+fixture(`Maps tests`)
+  .page(testEnvironment.feUrl)
+  .beforeEach(async () => {
+    await loginPage.loginAsPrinciple();
+    await mapsPage.navigateToMapsPage();
+  });
+
+test('clicking on send file after all fields fill', async () => {
+  await mapsPage.openPopUpAddButton();
+  await mapsPage.selectFile();
+  await mapsPage.typeFileName('file name 1');
+  await mapsPage.clickToUploadFile();
+
+  await mapsPage.expectPopUpToBeClosed();
+});
+
+test('clicking on cancle should close the windows', async () => {
+  await mapsPage.openPopUpAddButton();
+  await mapsPage.clickToClosePopUp();
+
+  await mapsPage.expectPopUpToBeClosed();
+});
+
+test('click on send file is available if all fields fill', async () => {
+  await mapsPage.openPopUpAddButton();
+  await mapsPage.selectFile();
+  await mapsPage.typeFileName('file name 2');
+
+  await t.expect(Selector('[id$="send-file"]').hasAttribute('disabled')).notOk();
+});
+
+test('click on send file is unavailable because file is missing', async () => {
+  await mapsPage.openPopUpAddButton();
+  await mapsPage.typeFileName('file name 3');
+
+  await t.expect(Selector('[id$="send-file"]').hasAttribute('disabled')).ok();
+});
+
+test('click on send file is unavailable because file name is missing', async () => {
+  await mapsPage.openPopUpAddButton();
+  await mapsPage.selectFile();
+
+  await t.expect(Selector('[id$="send-file"]').hasAttribute('disabled')).ok();
+});
---
+++ tests/pageobjects/maps.po.ts
@@ -0,0 +1,35 @@
+import { Selector, t } from 'testcafe';
+
+export default class MapsPage {
+  constructor() {}
+
+  public async navigateToMapsPage() {
+    await t
+      .click('[id$="navbarDropdownMenuLink0"]')
+      .wait(20)
+      .click('.nb-manage-maps');
+  }
+
+  public async expectPopUpToBeClosed() {
+    const count = await Selector('.add-map-modal', { visibilityCheck: true }).count;
+    await t.expect(count).eql(0);
+  }
+
+  public async openPopUpAddButton() {
+    await t.click('[data-test-id$="add-map-button"]');
+  }
+
+  public async selectFile() {
+    await t.setFilesToUpload('#add-picture', ['./uploads/1.jpg']);
+  }
+  public async clickToClosePopUp() {
+    await t.click('[id$="cancle"]').wait(1000);
+  }
+
+  public async clickToUploadFile() {
+    await t.click('[id$="send-file"]').wait(1000);
+  }
+  public async typeFileName(fileName) {
+    await t.typeText('#map-name', fileName);
+  }
+}
